62ae7b05180b182831f03c995aeee91d214361df
Elyas <Elyas.shamal2@gmail.com>
2023-07-09 15:06:26 -0400

    function-python

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,35 @@
 #!/usr/bin/env python3
 
+
 def greet_programmer():
-    pass
+    print(f"Hello, programmer!")
+
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+
+
+"""
+also we can 
+def add(num1, num2):
+result = sum1 + sum2
+print(result)
+"""
+
 
 def halve(number):
-    pass
+    return number / 2
+
+
+"""
+def halve(number= 100):
+return number / 2
+"""
